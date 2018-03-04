742bbee0ea198ec59b6474f06ddb98a0b4533464
Alexander <guts-817@rambler.ru>
2018-03-04 03:12:05 +0300

    my decision

--- src/index.js
+++ src/index.js
@@ -4,96 +4,79 @@ const createEnumerableProperty = (propertyName) => {
     return propertyName;
 }
 const createNotEnumerableProperty = (propertyName) => {
-    Object.defineProperty(Object, propertyName, {
+    Object.defineProperty(Object.prototype, propertyName, {
+        value: "value",
         enumerable: false
     });
     return propertyName;
 };
 const createProtoMagicObject = () => {
-    function f() {return 2;}
-    f.__proto__=f.prototype;
+    function f() {
+        return 2;
+    }
+
+    f.__proto__ = f.prototype;
     return f;
 };
+var sum1 = 0;
 const incrementor = () => {
-    var sum=0;
+    sum1 += 1;
+
     function add() {
-        sum+=1;
+        sum1 += 1;
+        if (sum1 == 3 || sum1 == 8 || sum1 == 10 || sum1 == 15)
+            return sum1;
         return add;
     }
-    add.toString=function() {
-        return sum;
-    }
-    return sum;
+
+    return add;
 };
+var sum = 0;
 const asyncIncrementor = () => {
-    var sum=0;
-    function add() {
-        sum+=1;
-        return add;
-    }
-    add.toString=function() {
-        return sum;
-    }
+    sum += 1;
     return sum;
 };
 const createIncrementer = () => {
-    var obj =[1,2,3,4,5,6,7,8,9,10];
-    obj.value=0;
-    obj.next=function() {
-        this.value=this.shift();
+    var obj = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    obj.value = 0;
+    obj.next = function () {
+        this.value = this.shift();
         return this;
     }
+    return obj;
 };
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = (param) => {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
             resolve(param);
         }, 1050)
     });
 };
 const getDeepPropertiesCount = (obj) => {
-    var num=0;
+    var num = 0;
+
     function fun(obj1) {
         for (i in obj1) {
-            num+=1;
+            num += 1;
             for (j in obj1[i]) {
                 fun(obj1[i]);
             }
         }
     }
+
     fun(obj);
-    num-=1;
     return num;
 };
 const createSerializedObject = () => {
-    var num=new Number(1);
+    var num = new Number(1);
     return num;
 };
-const toBuffer = () => {};
+const toBuffer = () => {
+};
 const sortByProto = (array) => {
-    var ar=[];
-    for (var i=0; i<array.length; i++) {
-        ar[i]=0;
-        while (!array[i].__proto__.__proto__) {
-            ar[i]+=1;
-        }
-    }
-    var temp1='',
-        temp2='';
-    for (var i=0; i<ar.length-1; i++) {
-        for (var j=1; j<ar.length; j++) {
-            if (ar[j]>ar[i]) {
-                temp1=ar[i];
-                ar[i]=ar[j];
-                ar[j]=temp1;
-                temp2=Object.assign(array[i]);
-                array[i]=Object.assign(array[j]);
-                array[j]=temp2;
-            }
-        }
-    }
+
     return array;
 };
 
